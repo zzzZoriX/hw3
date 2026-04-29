b290451bae822ebd169c19c0b3efed7a5132a369
anyway <zorix228337@gmail.com>
2026-04-29 18:04:39 +0300

    добавил кнопки

---
+++ index.js
@@ -0,0 +1,27 @@
+function goToTask1() {
+    window.location.href = "task1.html";
+}
+
+function goToTask2() {
+    window.location.href = "task2.html";
+}
+
+function goToTask3() {
+    window.location.href = "task3.html";
+}
+
+function goToTask4() {
+    window.location.href = "task4.html";
+}
+
+function goToTask5() {
+    window.location.href = "task5.html";
+}
+
+function goToTask6() {
+    window.location.href = "task6.html";
+}
+
+function goToTask7() {
+    window.location.href = "task7.html";
+}
